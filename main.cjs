2846acf72627f92359679983f677fc25dc6f90ea
afwan-belang <muhammadhaikalafwan@gmail.com>
2026-05-11 09:09:40 +0700

    asdfdsf

---
+++ main.cjs
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    minWidth: 800,
+    minHeight: 600,
+    autoHideMenuBar: true, // Menyembunyikan menu bar bawaan agar lebih rapi
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  });
+
+  // Memuat file hasil build React
+  win.loadFile(path.join(__dirname, 'dist', 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
